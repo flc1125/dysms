8d67f0fce22bb42b7b409fb13e8c53c7092a08f6
Flc <four_leaf_clover@foxmail.com>
2017-07-19 00:25:12 +0800

    client

--- src/Client.php
+++ src/Client.php
@@ -3,6 +3,8 @@ namespace Flc\Dysms;
 
 use Flc\Dysms\Request\IRequest;
 
+use Flc\Dysms\Helper;
+
 /**
  * dysms客户端类
  *
@@ -15,6 +17,18 @@ class Client
      * @var string
      */
     protected $api_uri = 'http://dysmsapi.aliyuncs.com/';
+
+    /**
+     * 回传格式
+     * @var string
+     */
+    protected $format = 'json';
+
+    /**
+     * 签名方式
+     * @var string
+     */
+    protected $signatureMethod = 'HMAC-SHA1';
 
     /**
      * 配置项
@@ -38,6 +52,26 @@ class Client
      */
     public function execute(IRequest $request)
     {
+        $action    = $request->getAction();
+        $reqParams = $request->getParams();
         return $request;
+    }
+
+    /**
+     * 公共返回参数
+     * @return array 
+     */
+    public function getPublicParams()
+    {
+        return [
+            'AccessKeyId'      => $this->config['accessKeyId'],
+            'Timestamp'        => date('Y-m-d\TH:i:s\Z'),
+            'Format'           => $this->format,
+            'SignatureMethod'  => $this->signatureMethod,
+            'SignatureVersion' => '1.0',
+            'SignatureNonce'   => Helper::uuid(),
+            'Version'          => '2017-05-25',
+            'RegionId'         => 'cn-hangzhou',
+        ];
     }
 }
